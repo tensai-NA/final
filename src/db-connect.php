a92a82078f97ea21a7939ab7a1203386fbdf2903
tensai-NA <aoi080815@gmail.com>
2024-01-10 10:12:43 +0900

    a

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+const SERVER = 'mysql220.phy.lolipop.lan';
+const DBNAME = 'LAA1517358-final';
+const USER = 'LAA1517358';
+const PASS = 'PAss0808';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
